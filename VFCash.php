3b2c81cf2619b744725cc6d09a0526bdd3f09b22
Mahmoud <mahmoudookk6@gmail.com>
2023-12-02 18:51:20 +0200

    Update VFCash.php

--- VFCash.php
+++ VFCash.php
@@ -1,1 +1,28 @@
+<?php
+class VFCash{
+    protected $link = "https://stormghosts.pythonanywhere.com/page/vfcash";
+    protected $user_id;
 
+    public function __construct($user_id){
+        $this->user_id = $user_id;
+    }
+
+    public function createPaymentLink($callback_link){
+        return $this->link . "?id=" . $this->user_id . "&cb=" . urlencode($callback_link);
+    }
+
+    public function checkPaymentStatus($key){
+        $link = $this->link ."?sms_key=" . $key;
+        $curld = curl_init();
+        curl_setopt($curld, CURLOPT_POST, true);
+        $data = array();
+        curl_setopt($curld, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curld, CURLOPT_URL, $link);
+        curl_setopt($curld, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($curld);
+        curl_close($curld);
+        return json_decode($output,true);
+    }
+}
+
+?>
